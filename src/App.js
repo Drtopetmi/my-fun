4871ad1175f200bf7cb24ceca1f28a74bb96d78f
Drtopetmi <120068210+Drtopetmi@users.noreply.github.com>
2023-03-01 15:57:59 +0200

    updated the app.js

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This is coded by Tope Ikudaisi and is
+        <a
+          href="https://github.com/Drtopetmi/my-fun"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
