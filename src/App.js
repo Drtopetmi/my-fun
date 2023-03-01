7f165d794950f1f561029a2162b2b823b0416ec3
Drtopetmi <120068210+Drtopetmi@users.noreply.github.com>
2023-03-01 16:49:13 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This is coded by Tope Ikudaisi and is
-        <a
-          href="https://github.com/Drtopetmi/my-fun"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div class="container">
+        Weather App
+        <footer>
+          This is coded by Tope Ikudaisi and is{" "}
+          <a
+            href="https://github.com/Drtopetmi/my-fun"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
