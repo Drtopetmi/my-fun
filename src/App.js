e3a55fcc61646e441905ec8e57f55b883a8588bf
Drtopetmi <120068210+Drtopetmi@users.noreply.github.com>
2023-03-10 23:17:57 +0200

    updated the default city

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Paris" />
+        <Weather defaultCity="London" />
         <footer>
           This is coded by Tope Ikudaisi and is{" "}
           <a
