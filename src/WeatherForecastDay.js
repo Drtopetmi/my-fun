7f6bca30a4f75968f7ed671cdc1ddeadcb8a8540
Drtopetmi <120068210+Drtopetmi@users.noreply.github.com>
2023-03-10 23:19:44 +0200

    updated forecast day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -16,7 +16,7 @@ export default function WeatherForecastDay(props) {
   function day() {
     let date = new Date(props.data.dt * 1000);
     let day = date.getDay();
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
     return days[day];
   }
   return (
